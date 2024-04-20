9ae2c8eaab2e4a0465d74a7f937f1b4736dcf374
its-ritu <paranjaperucha9@gmail.com>
2024-04-20 21:22:49 +0530

    initial commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,13 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import openai from './openai.svg';
+import openai2 from './openai2.png'
+
+export {
+  download,
+  logo,
+  openai,
+  preview,
+  openai2
+};
